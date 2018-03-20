0c8afbda2d9a7829812609ea82b5607348cd48c4
huangyunfei <1140617436@qq.com>
2018-03-20 10:39:29 +0800

    有数据图片了

--- src/main/java/cn/edu/nju/trainingcollege/controller/ManagerController.java
+++ src/main/java/cn/edu/nju/trainingcollege/controller/ManagerController.java
@@ -6,15 +6,16 @@ import cn.edu.nju.trainingcollege.entity.ChangeApprovalEntity;
 import cn.edu.nju.trainingcollege.entity.RegisterApprovalEntity;
 import cn.edu.nju.trainingcollege.entity.SchoolEntity;
 import cn.edu.nju.trainingcollege.service.ManagerService;
+import cn.edu.nju.trainingcollege.vo.DataVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping(value = "/manager")
 @Controller
@@ -91,4 +92,31 @@ public class ManagerController {
         model.addAttribute("changeapprovals",changeApprovalEntities);
         return "manager/changeapproval";
     }
+
+    @RequestMapping({"/statistics"})
+    public String showstatistics(Model model){
+
+        return "manager/statistics";
+    }
+
+    @GetMapping("/data")
+    @ResponseBody
+    public List<DataVo> data() {
+
+        List<DataVo> list=new ArrayList<>();
+        DataVo vo1=new DataVo(400,"我的电脑");
+        DataVo vo2=new DataVo(335,"直接访问");
+        DataVo vo3=new DataVo(310,"邮件营销");
+        DataVo vo4=new DataVo(274,"联盟广告");
+        DataVo vo5=new DataVo(235,"视频广告");
+
+        list.add(vo1);
+        list.add(vo2);
+        list.add(vo3);
+        list.add(vo4);
+        list.add(vo5);
+        return list;
+    }
+
+
 }
---
+++ src/main/java/cn/edu/nju/trainingcollege/vo/DataVo.java
@@ -0,0 +1,29 @@
+package cn.edu.nju.trainingcollege.vo;
+
+public class DataVo {
+    private int value;
+    private String name;
+
+    public DataVo(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public DataVo(){}
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
